a780ce1e5853fa9d261c17368cc20d96063ffa4c
Jordan Burke <jordan.burke@gmail.com>
2026-06-25 18:16:06 -0400

    feat(traced): export TracedOption, TracerLive, Tracer from functype package

---
+++ packages/functype/src/traced/index.ts
@@ -0,0 +1,4 @@
+export type { TraceEvent, TraceOp, TraceSpan, SpanOutcome } from "./TraceEvent.js"
+export { Tracer } from "./Tracer.js"
+export { TracerLive } from "./TracerLive.js"
+export { TracedOption } from "./TracedOption.js"
